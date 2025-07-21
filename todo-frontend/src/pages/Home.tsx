5bde500eaa402cb5de94ac7bcb3b61bb850f8a45
Ashton Jurkowska <Ashton.Jurkowska@knowit.eu>
2025-07-21 10:03:35 +0200

    Small fixes to TodoItem and Home components

--- todo-frontend/src/pages/Home.tsx
+++ todo-frontend/src/pages/Home.tsx
@@ -2,34 +2,34 @@ import React, { useState } from 'react';
 import TodoItem from '../components/TodoItem';
 
 const Home: React.FC = () => {
-    const [todos, setTodos] = useState([
-        { id: 1, title: 'Learn React', completed: false },
-        { id: 2, title: 'Build a Todo App', completed: false },
-        { id: 3, title: 'Deploy the App', completed: false },
-    ]);
+  const [todos, setTodos] = useState([
+    { id: 1, title: 'Learn React', completed: false },
+    { id: 2, title: 'Build a Todo App', completed: false },
+    { id: 3, title: 'Deploy the App', completed: false },
+  ]);
 
-    const toggleTodo = (id: number) => {
-        setTodos(todos.map(todo => 
-            todo.id === id ? { ...todo, completed: !todo.completed } : todo
-        ));
-    };
+  const toggleTodo = (id: number) => {
+    setTodos(todos.map(todo =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    ));
+  };
 
-    return (
-        <div>
-            <h1>Todo List</h1>
-            <ul>
-                {todos.map(todo => (
-                    <TodoItem 
-                        key={todo.id} 
-                        id={todo.id} 
-                        title={todo.title} 
-                        completed={todo.completed} 
-                        toggleTodo={toggleTodo} 
-                    />
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map(todo => (
+          <TodoItem
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            completed={todo.completed}
+            toggleTodo={toggleTodo}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
